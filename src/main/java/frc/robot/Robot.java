64f484d0f2fd6eeab4fb34bc8b844d33e2bb8548
green726 <mguinangreen726@gmail.com>
2022-01-26 10:40:52 -0500

    fixed imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,9 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.OldSwerveDriveTrain;
 import frc.robot.subsystems.SwerveDriveTrain;
-import frc.robot.commands.OldSwerveCommand;
 //import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.util.IO;
 
