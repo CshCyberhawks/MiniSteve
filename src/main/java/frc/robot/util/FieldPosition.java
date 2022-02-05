78be9b823cd8b0c54756db195d7228df17ad59a4
PY44N <pyan4444@gmail.com>
2022-02-05 17:46:06 -0500

    Things

--- src/main/java/frc/robot/util/FieldPosition.java
+++ src/main/java/frc/robot/util/FieldPosition.java
@@ -1,9 +1,6 @@
 package frc.robot.util;
-import java.util.HashMap;
-import frc.robot.util.Vector2;
 
 public class FieldPosition {
-    private static double angle = 0;
     private static Vector2 positionCoord = new Vector2();
 
     private static Gyro gyro = new Gyro();
