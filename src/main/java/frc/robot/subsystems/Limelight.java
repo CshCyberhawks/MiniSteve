de4ecb6b69952745b4432de03fb72b889cfa0402
Buster <busterc246@gmail.com>
2022-02-06 14:58:19 -0800

    Changed some smart dashboard things and added a commented out distance calculation

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -10,11 +10,12 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class Limelight extends SubsystemBase {
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
     NetworkTableEntry tv = table.getEntry("tv"); //Between 0 and 1 whether it has a valid target
-    NetworkTableEntry tx = table.getEntry("tx"); //The horizontal offset between the crosshair and target
-    NetworkTableEntry ty = table.getEntry("ty"); //The vertical offset between the crosshair and target
+    NetworkTableEntry tx = table.getEntry("tx"); //The horizontal offset between the crosshair and target in degrees
+    NetworkTableEntry ty = table.getEntry("ty"); //The vertical offset between the crosshair and target in degrees
     NetworkTableEntry ta = table.getEntry("ta"); //Percentage of image (filled by target?)
     NetworkTableEntry tcornx = table.getEntry("tcornx");
     NetworkTableEntry tcorny = table.getEntry("tcorny");
+    NetworkTableEntry tcornxy = table.getEntry("tcornxy");
     
     public Limelight() {
         
@@ -26,14 +27,19 @@ public class Limelight extends SubsystemBase {
         double horrizontalOffset = tx.getDouble(0.0);
         double verticalOffset = ty.getDouble(0.0);
         double area = ta.getDouble(0.0);
-        double[] xCorners = tcornx.getDoubleArray(new double[4]);
-        double[] yCorners = tcorny.getDoubleArray(new double[4]);
+        double[] xCorners = tcornx.getDoubleArray(new double[2]);
+        double[] yCorners = tcorny.getDoubleArray(new double[2]);
+        double[] xyCorners = tcornxy.getDoubleArray(new double[4]);
+        
+        //Values needed from final robot before implemented
+        //double distanceFromTarget = (targetHeight - cameraHeight) / Math.tan(mountAngle + verticalOffset);
 
-        SmartDashboard.putNumber("hasValidTarget", hasValidTarget);
-        SmartDashboard.putNumber("limelight horrizontalOffset", horrizontalOffset);
-        SmartDashboard.putNumber("limelight verticalOffset", verticalOffset);
+        SmartDashboard.putNumber("Limelight hasValidTarget", hasValidTarget);
+        SmartDashboard.putNumber("Limelight horrizontalOffset", horrizontalOffset);
+        SmartDashboard.putNumber("Limelight verticalOffset", verticalOffset);
         SmartDashboard.putNumber("LimelightArea", area);
-        SmartDashboard.putNumberArray("xCorners", xCorners);
-        SmartDashboard.putNumberArray("yCorners", yCorners);
+        SmartDashboard.putNumberArray("Limelight xCorners", xCorners);
+        SmartDashboard.putNumberArray("Limelight yCorners", yCorners);
+        SmartDashboard.putNumberArray("Limelight xyCorners", xyCorners);
     }
 }
