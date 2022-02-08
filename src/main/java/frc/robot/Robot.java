7a0d85ca7391d4da5c71ac961a12ffe7bc959357
green726 <mguinangreen726@gmail.com>
2022-02-08 14:15:45 -0500

    stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -123,6 +123,8 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
+    swo.updatePosition();
+
   }
 
   @Override
