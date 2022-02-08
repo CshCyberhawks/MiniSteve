d10e97ceebe58ce64cf73aacde8fb2a4b2eb48cc
green726 <mguinangreen726@gmail.com>
2022-02-08 14:18:25 -0500

    comment

--- src/main/java/frc/robot/commands/AutoCommandGroup.java
+++ src/main/java/frc/robot/commands/AutoCommandGroup.java
@@ -7,6 +7,9 @@ import frc.robot.Robot;
 public class AutoCommandGroup extends SequentialCommandGroup{
     public AutoCommandGroup() {
         Robot.swo.resetPos();
+        //add your autonomous commands below
+        //example: below will move robot 2 meters on the x and rotate to 90 degrees
+        //then it will wait 1 second before moving the robot back to its starting position
         addCommands(
             new AutoGoTo(new FieldPosition(2, 0, 90)),
             new Wait(1000),
