9e7024a82d863378aa0508ca201b6b05db9162fb
Smudge <87445504+Smubge@users.noreply.github.com>
2022-02-07 20:28:38 -0500

    Update ClimbSystem.java

--- src/main/java/frc/robot/subsystems/ClimbSystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSystem.java
@@ -6,14 +6,18 @@ import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSystem extends SubsystemBase{
-    private CANSparkMax climbMotor;
+    private CANSparkMax leftclimbMotor;
+    private CANSparkMax rightclimbMotor;
     
     public ClimbSystem()
     {
-        climbMotor = new CANSparkMax(0,CANSparkMaxLowLevel.MotorType.kBrushless);
+        leftclimbMotor = new CANSparkMax(0,CANSparkMaxLowLevel.MotorType.kBrushless);
+        rightclimbMotor = new CANSparkMax(0,CANSparkMaxLowLevel.MotorType.kBrushless);
+
     }
     public void climb(double speed)
     {
-        climbMotor.set(speed);
+        leftclimbMotor.set(speed);
+        rightclimbMotor.set(speed);
     }
 }
