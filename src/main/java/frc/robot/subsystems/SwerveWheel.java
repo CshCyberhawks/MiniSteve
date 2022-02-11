0f22112202d9fc1e15ff84838bc1df2eea71d74a
PY44N <pyan4444@gmail.com>
2022-02-10 19:38:16 -0500

    Things

--- src/main/java/frc/robot/subsystems/SwerveWheel.java
+++ src/main/java/frc/robot/subsystems/SwerveWheel.java
@@ -66,7 +66,7 @@ public class SwerveWheel {
     }
 
     public void drive(double speed, double angle) {
-        speed = convertToMetersPerSecond(speed * 3000); //Converting the speed to m/s with a max rpm of 3000 (GEar ratio is 7:1)
+        speed = convertToMetersPerSecond(speed * 3000); //Converting the speed to m/s with a max rpm of 3000 (Gear ratio is 7:1)
 
         SmartDashboard.putNumber(m_turnEncoderPort + " angle input", angle);
         SmartDashboard.putNumber(m_turnEncoderPort + " speed input", speed);
