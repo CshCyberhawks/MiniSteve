79e5dfacb24e1db460106ba7886ab2f7e256b52c
PY44N <pyan4444@gmail.com>
2022-03-09 21:40:47 -0500

    Made it log when it is broken

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.SwerveCommand;
@@ -17,64 +18,81 @@ import edu.wpi.first.cameraserver.CameraServer;
 //import frc.robot.subsystems.SwerveSubsystem;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
   // private DriveSystem driveSystem;
   private Command m_autonomousCommand;
   // private Alliance teamColor;
-  //private OldSwerveDriveTrain swerveSystem;
+  // private OldSwerveDriveTrain swerveSystem;
   private SwerveDriveTrain swerveSystem;
-  private int IRSensorPort = 20; //something - DIO
-  private DigitalInput sensor; 
+  private int IRSensorPort = 1; // something - DIO
+  private DigitalInput sensor;
   private boolean lastState;
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
     // autonomous chooser on the dashboard.
-    teamColor = DriverStation.getAlliance();
+    // teamColor = DriverStation.getAlliance();
     CameraServer.startAutomaticCapture();
     // m_robotContainer = new RobotContainer();
-    
-    //driveSystem = new DriveSystem();
+
+    // driveSystem = new DriveSystem();
     swerveSystem = new SwerveDriveTrain();
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and
+   * test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
-    //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
@@ -84,7 +102,8 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
   public void teleopInit() {
@@ -97,23 +116,24 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-      
-    
+    sensor = new DigitalInput(IRSensorPort);
+
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() { // KitKat
-    //Beam Break Code
-    sensor = new DigitalInput(IRSensorPort);
+    // Beam Break Code
     // check if the sensor beam is broken
     // if it is, the sensorState is LOW:
     boolean sensorState = sensor.get();
-    if (sensorState && !lastState) { 
-      //it's unbroken!
-    } 
+    if (sensorState && !lastState) {
+      // it's unbroken!
+      SmartDashboard.putBoolean("Broken", false);
+    }
     if (!sensorState && lastState) {
-      //it's broken!
+      // it's broken!
+      SmartDashboard.putBoolean("Broken", true);
     }
     lastState = sensorState;
   }
@@ -122,11 +142,11 @@ public class Robot extends TimedRobot {
   public void testInit() {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
-    
 
   }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 }
