960755e724baa026d32a8555a87d1779b387a39c
green726 <59667714+green726@users.noreply.github.com>
2022-03-20 17:51:51 -0400

    added time monitors to stored cargo

--- src/main/java/frc/robot/subsystems/TransportSystem.java
+++ src/main/java/frc/robot/subsystems/TransportSystem.java
@@ -6,19 +6,34 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
+import frc.robot.util.MathClass;
 
 public class TransportSystem extends SubsystemBase {
     private VictorSPX transportMotor;
     private final double traversalMult = 2;
     private int cargoAmount = 0;
     private boolean isRunningSequence;
+    private double lastCargoPickupTime = 0;
+    private double lastCargoShootTime = 0;
+
+    private boolean cargoPickedUp = false;
+    private boolean cargoShot = false;
 
     @Override
     public void periodic() {
-        if (!Robot.getFrontBreakBeam().get()) {
+
+        double shootDifference = MathClass.getCurrentTime() - lastCargoShootTime;
+        double pickupDifference = MathClass.getCurrentTime() - lastCargoPickupTime;
+
+        cargoPickedUp = !Robot.getFrontBreakBeam().get() && cargoAmount < 2 && pickupDifference > 60;
+        cargoShot = !Robot.getShootBreakBeam().get() && cargoAmount > 0 && shootDifference > 60;
+
+        if (cargoPickedUp) {
+            lastCargoPickupTime = MathClass.getCurrentTime();
             cargoAmount++;
         }
-        if (!Robot.getShootBreakBeam().get()) {
+        if (cargoShot) {
+            lastCargoShootTime = MathClass.getCurrentTime();
             cargoAmount--;
         }
     }
